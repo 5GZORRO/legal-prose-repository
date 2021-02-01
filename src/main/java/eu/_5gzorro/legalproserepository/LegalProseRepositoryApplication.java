4b6ec57845683a08a0f09a73f05409435a0f6c2c
James Taylor <jamestaylor@bartr.world>
2021-02-01 15:17:49 +0000

    feat(app): initial commit of empty spring boot app

---
+++ src/main/java/eu/_5gzorro/legalproserepository/LegalProseRepositoryApplication.java
@@ -0,0 +1,13 @@
+package eu._5gzorro.legalproserepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalProseRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalProseRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/_5gzorro/legalproserepository/LegalProseRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package eu._5gzorro.legalproserepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalProseRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
