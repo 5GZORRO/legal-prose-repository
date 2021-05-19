4c10fc7e3802434092948e58f870f44c8be2c423
James Taylor <jamestaylor@bartr.world>
2021-05-19 11:00:52 +0100

    fix(cors/ingress): added cors allow all origins and ingress config to fix swagger-ui on stage

---
+++ src/main/java/eu/_5gzorro/legalproserepository/config/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package eu._5gzorro.legalproserepository.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
+
